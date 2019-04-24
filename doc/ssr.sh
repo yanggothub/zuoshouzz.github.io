4a627fe5d4dfa6a985e24ff26e2dd5cc92fec8b3
zuoshouzz <285678728@qq.com>
2019-04-24 21:47:12 +0800

    Site updated: 2019-04-24 21:47:12

--- doc/ssr.sh
+++ doc/ssr.sh
@@ -245,8 +245,8 @@ Set_config_port(){
 	while true
 	do
 	echo -e "请输入要设置的ShadowsocksR账号 端口"
-	read -e -p "(默认: 2333):" ssr_port
-	[[ -z "$ssr_port" ]] && ssr_port="2333"
+	read -e -p "(默认: 443):" ssr_port
+	[[ -z "$ssr_port" ]] && ssr_port="443"
 	echo $((${ssr_port}+0)) &>/dev/null
 	if [[ $? == 0 ]]; then
 		if [[ ${ssr_port} -ge 1 ]] && [[ ${ssr_port} -le 65535 ]]; then
